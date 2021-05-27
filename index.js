98c0a69d6d1ef802729c4a9ce3180b97dc3ea8f0
Sydney Sisco <hello@sydsis.co>
2021-05-26 18:11:24 -0700

    get info before and after event

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('voiceStateUpdate', msg => {
-  console.log('voiceStateUpdate:', msg)
+client.on('voiceStateUpdate', (oldState, newState) => {
+  console.log('voiceStateUpdate old:', oldState);
+  console.log('voiceStateUpdate new:', newState);
 
+  if (!oldState.channelID && newState.channelID) {
+    // add role
+    // process.env.MUMBLE_ROLE_ID
+    return;
+  }
+
+  if (oldState.channelID && !newState.channelID) {
+    // remove role
+
+    return;
+  } 
 
 });
 
