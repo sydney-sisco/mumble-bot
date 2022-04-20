5d202611e5a98c61a1ef5286237663cfe8ea1b75
Sydney Sisco <sydney@sydneys-air.lan>
2022-04-19 21:08:46 -0700

    deletes messages when triggers manually

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A quick little Discord bot that adds a Role to all users active in a voice channel. Uses [Discord.js](https://www.npmjs.com/package/discord.js). To run this yourself, create a `.env` file using the example as a reference. You'll need a Discord bot token. Building your first Discord bot? I wrote a [tutorial just for you](https://sydney-sisco.medium.com/build-and-host-your-first-discord-bot-using-node-js-and-aws-20607585b6e3)!
 
 ![mumble icon](https://github.com/sydney-sisco/mumble-bot/blob/main/mumble-icon.png?raw=true)
+
+
+## Development
+
+`node index.js`
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const PREFIX = "!m";
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -26,6 +28,30 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     return;
   } 
 
+  
+});
+
+client.on('message', async message => {
+
+  console.log('message received');
+  // ignore messages sent by bots
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content.startsWith(PREFIX)) {
+    const channel = message.channel; // TextChannel object
+    const messageManager = channel.messages; // MessageManager object
+
+    messageManager.fetch({ limit: 100 }).then((messages) => {
+      // `messages` is a Collection of Message objects
+      messages.forEach((message) => {
+        message.delete();
+      });
+
+      channel.send("100 messages have been deleted!");
+    });
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
